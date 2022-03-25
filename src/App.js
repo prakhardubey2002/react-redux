7cc369f79b6517279f25f6de8df485077a45b0c1
Prakhar Dubey <prakhardubey2002@gmail.com>
2022-03-25 09:34:17 +0530

    Counter Project UI created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    
+      <h1>Increment/Decrement Counter</h1>
+      <div className="quantity">
+        <a className='quantity-minus' title='Decrement' >
+          <span>-</span>
         </a>
-      </header>
+        <input name='quantity' type="text" className='quantity_input' value="0" />
+        <a className='quantity_plus' title='increment'>
+          <span>+</span>
+        </a>
+      </div>
     </div>
   );
 }
